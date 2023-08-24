c82e589cec6090ca6a1d54ae3cf0d807645cc675
JJMorales <jjmorales.dev@gmail.com>
2023-08-24 18:07:33 -0500

    feat: :sparkles: agregado middlware de autenticacion

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,28 +1,24 @@
+import { request, response } from "express";
 import { verifyToken } from "../utils/jwt.js";
 
-export const authRequired = async (req, res, next) => {
+export const authRequired = async (req = request, res = response, next) => {
   try {
     const { token } = req.cookies;
     const { authorization } = req.headers;
-
-    console.log({ token });
-    console.log({ authorization });
-
     if (!authorization && !token) {
       return res
         .status(401)
         .json({ message: "no token, authorization denied" });
     }
 
-    const dataToken = await verifyToken(token || authorization);
-
-    if (!dataToken) {
+    const payload = await verifyToken(token || authorization);
+    if (!payload) {
       return res
         .status(401)
         .json({ message: "token invalid, authorization denied" });
     }
 
-    req.user = dataToken;
+    req.user = payload;
     next();
   } catch (error) {
     res.status(501).json(error);
