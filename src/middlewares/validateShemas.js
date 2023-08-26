252fa2552a35bd1277ee907978f5c9b8881697a6
JJMorales <jjmorales.dev@gmail.com>
2023-08-26 00:14:55 -0500

    feat: :sparkles: agregue validacion de esquemas

---
+++ src/middlewares/validateShemas.js
@@ -0,0 +1,15 @@
+import { request, response } from "express";
+
+export const validatorHandler = (schema, property) => {
+  return async (req = request, res = response, next) => {
+    try {
+      const data = req[property];
+      const body = await schema.validateAsync(data, { abortEarly: false });
+      console.log("🚀 ~ file: validateShemas.js:8 ~ return ~ body:", body);
+      next();
+    } catch (error) {
+      console.log("🚀 ~ file: validateShemas.js:11 ~ return ~ error:", error);
+      return res.status(400).json({ errors: error });
+    }
+  };
+};
